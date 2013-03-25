752a3708f618f52d6c4062dc66f0604b22ad6823
Dan Fairs <dan@fezconsulting.com>
2013-03-25 16:57:56 +0000

    Another attempt to fix Travis build

--- testproject/manage.py
+++ testproject/manage.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.settings")
 
     from django.core.management import execute_from_command_line
-
+    sys.path.append('..')
     execute_from_command_line(sys.argv)
